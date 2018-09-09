fdefde7e6756871ee9750118329c8206af8fe7ef
Don-Vincent Snell <39629030+DVSnell@users.noreply.github.com>
2018-09-08 20:14:43 -0600

    Added hw-3

---
+++ hw-3/sketch.js
@@ -0,0 +1,16 @@
+// Setup
+function setup(){
+
+    //640x480 px canvas creation command
+    createCanvas(640, 480);
+
+    //Background of our 640x480 canvas set to yellow
+    background(255, 210, 0);
+}
+
+//Adding Draw function
+function draw(){
+
+    //Text function, centered on my background
+    text("Hello World!", 320, 240)
+}
